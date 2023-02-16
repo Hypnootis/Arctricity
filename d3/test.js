89a0292208d534845af8f5f86b9e452fd5dca311
Hypnootis <tm.pasanen@outlook.com>
2023-02-16 10:44:52 +0200

    Trying out d3

--- d3/test.js
+++ d3/test.js
@@ -1,24 +1,9 @@
-const DUMMY_DATA = [
-    {id: "d1", value: 10, region: "USA"},
-    {id: "d2", value: 11, region: "India"},
-    {id: "d3", value: 12, region: "China"},
-    {id: "d4", value: 13, region: "Germany"},
+import {Histogram} from "@d3/histogram";
 
-];
+const VALUES = [-3.14, -0.01, 3.76];
 
+let myGram = Histogram(VALUES);
 
-d3.select("#data")
-    .style("border", "1px solid red")
-    .classed("container", true);
-
-container
-    .selectAll(".bar")
-    .data(DUMMY_DATA)
+d3.select("div")
     .enter()
-    .append("div")
-    .classed("bar", true)
-    .style("background-color", "#720570")
-    .style("width", "50px")
-    .style("height", "150px")
-    .style("display", "flex")
-    .style("justify-content", "around");
+    .append(myGram);
