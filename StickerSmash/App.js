937c5e2134ec4324c8fcc20373d1805c26e49322
Hypnootis <tm.pasanen@outlook.com>
2023-02-28 01:13:04 +0200

    Added StickerSmash example

---
+++ StickerSmash/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={{uri: "https://docs.expo.dev/static/images/tutorial/background-image.png"}}
+        style={{width: 400, height: 400}}/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
