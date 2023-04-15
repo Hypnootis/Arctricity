31481147bf67e9141240d3cc24259cb5379b69b6
Hypnootis <tm.pasanen@outlook.com>
2023-04-15 22:41:30 +0300

    Graphs example works!

--- client/src/api/readDatabase.mjs
+++ client/src/api/readDatabase.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection, query, where, limit } from "firebase/firestore";
+import { getFirestore, getDocs, getDoc, getDocFromServer, collection, query, where, limit, doc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDGnSle1ZEp6p-qp74ksFFYzGfh9g_Ouzo",
@@ -25,7 +25,34 @@ if (docSnap.exists()) {
 
 */
 // TODO: Create function to query for a specified time range
-let timeframe = "Sat Apr 01 2023 17:"; // Query string
+let timeframe = "Apr 01 2023 17:"; // Query string
+
+/*
+function weekdayName(day, month) {
+    let baseDate = new Date(Date.UTC(2023, month, day));
+    return baseDate.toLocaleDateString("en-US", {weekday: "short"});
+}
+
+function formatTime(day, month, hour) {
+    let date = new Date(day, month, hour);
+    return date; 
+}
+*/
+
+//console.log(formatTime(1, 4, 12));
+
+async function getDocument(db, id) {
+    const snap = await getDocFromServer(doc(db, "Data", id));
+    if (snap.exists()) {
+        console.log("Document data: ", snap.data());
+    } else {
+        return Promise.reject(Error("No such document: Data.${id}"));
+    }
+}
+getDocument(db, "2023_04_12_18" + '\uf8ff');
+
+
+/*
 
 const docsRef = collection(db, "Data");
  // Time-range query
@@ -36,7 +63,7 @@ const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
     console.log(doc.id, " => ", doc.data());
 });
-
+/*
 // Single doc query
 /*
 const q = query(docsRef, where("time", "==", "Wed Mar 01 2023 12:09:47 GMT+0200 (GMT+02:00)"), limit(1));
--- client/src/interface/Graphs.jsx
+++ client/src/interface/Graphs.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card, Title, Paragraph } from "react-native-paper";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions, ScrollView } from "react-native";
 import { styling } from "./MyTheme";
+import { BarChart, LineChart } from "react-native-chart-kit";
 
 const data_file= require('./test_data.json');
 const data = data_file.test;
@@ -9,13 +10,55 @@ const data = data_file.test;
 export const Graphs = () => {
     return (
         <View>
+            <ScrollView>
             <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Status as of:{'\n'}{data.time}</Text>
             <Card>
                 <Card.Content style={styling.cards}>
                     <Title>This could have a cool graph</Title>
-                    <Paragraph>Mesa</Paragraph>
+                    <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
                 </Card.Content>
-                <Card.Cover source={require("../../assets/mesa.jpeg")} />
             </Card>
             <Card>
                 <Card.Content style={styling.cards}>
@@ -24,7 +67,7 @@ export const Graphs = () => {
                 </Card.Content>
                 <Card.Cover source={require("../../assets/tummat.jpeg")} />
             </Card>
-
+    </ScrollView>
     </View>
     );
 }
